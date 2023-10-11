dbc97c8bd9d0451a08994048422c741ae3653acb
michelle <michelle.graham@dkit.ie>
2023-10-11 01:41:31 +0100

    Added example of tracking a running total without using loops

---
+++ Week 4 - Loops/unlooped_running_total.py
@@ -0,0 +1,9 @@
+total = 0
+
+total = total + int(input("Please enter #1: "))
+total = total + int(input("Please enter #2: "))
+total = total + int(input("Please enter #3: "))
+total = total + int(input("Please enter #4: "))
+total = total + int(input("Please enter #5: "))
+
+print("The total is:", total)
