f5d2a1659a98ad2096c1b171a10053b4893f2bab
michelle <michelle.graham@dkit.ie>
2023-12-05 17:07:22 +0000

    Inserted solution to problem (finding longest name and corresponding ID)

--- Week 11 - File IO & Structured Data/sample_structured_read_storing_for_manipulation.py
+++ Week 11 - File IO & Structured Data/sample_structured_read_storing_for_manipulation.py
@@ -6,12 +6,19 @@ with open(filename) as file_handle:
         line = line.strip()
         components = line.split("%%")
         if len(components) == 2:
-            print(f"Student number: {components[0]}")
-            student_names.append(components[0])
-            print(f"Student name: {components[1]}")
-            student_ids.append(components[1])
 
+            student_ids.append(components[0])
+            student_names.append(components[1])
+
+longest_pos = 0
 # Locate student with longest name
+for i in range(len(student_names)):
+    if len(student_names[i]) > len(student_names[longest_pos]):
+        longest_pos = i
+        
+print(f"Longest name: {student_names[longest_pos]}")
+print(f"Student ID: {student_ids[longest_pos]}")
+    
 # Print out their name and their ID
 # Where two students have the same length as longest,
 # Print the info of the first one
