bb1bb54e5faf7dc75459956c0db2d92318b2ca99
michelle <michelle.graham@dkit.ie>
2024-01-29 09:48:00 +0000

    Added pseudocode to count the number of times a specific value appears in a list

---
+++ S2_Week 02 - Problem Solving/counting_element_in_list_pseudocode.py
@@ -0,0 +1,10 @@
+input: 	List of floats (grades)
+        Value to be counted (float)
+        
+Pseudocode:
+Create counter for value and set to 0
+for each grade in the grades list
+    Check if current grade equals value being counted
+        If so, add ONE to counter for value
+
+Display counter for this value
