5a1d44a496d1d25b4b49c299d497d2c80d4314e6
michelle <michelle.graham@dkit.ie>
2024-03-04 10:32:30 +0000

    Added examples of looping through various parts (keys, values, items) of a dictionary

--- S2_Week 07 - Keyed Sorting and Dictionaries/working_with_dictionaries.py
+++ S2_Week 07 - Keyed Sorting and Dictionaries/working_with_dictionaries.py
@@ -14,7 +14,17 @@ if "student 6" in students:
     print(students["student 6"])
 else:
     print("\"student 6\" is not a recognised tag/key")
-        
+
+# Display each of the keys stored in the dictionary
+for i, name in enumerate(students.keys()):
+    print(f"{i} {name}") 
+
+# Display each of the values (student tuples) stored in the dictionary
+for i, student in enumerate(students.values()):
+    print(f"{i} {student}")
+
+# Display each of the key-value pairs (student name & its associated tuple)
+# stored in the dictionary
 for i, (name, student) in enumerate(students.items()):
     print(f"{i} {name}: {student}")
     
