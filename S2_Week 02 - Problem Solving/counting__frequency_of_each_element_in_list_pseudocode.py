6fc4e0ae4e290fbc3cef21613a15bb67382c3b18
mgraham-dkit <126207423+mgraham-dkit@users.noreply.github.com>
2024-03-08 18:29:39 +0000

    Update counting__frequency_of_each_element_in_list_pseudocode.py

--- S2_Week 02 - Problem Solving/counting__frequency_of_each_element_in_list_pseudocode.py
+++ S2_Week 02 - Problem Solving/counting__frequency_of_each_element_in_list_pseudocode.py
@@ -2,10 +2,10 @@ input: 	List of floats (grades)
         Value to be counted (float)
         
 Pseudocode:
-for each value in grades list
+for each value in grades list:
     Create counter for value and set to 0
-    for each grade in the grades list
-        Check if current grade equals value being counted
-            If so, add ONE to counter for value
+    for each grade in the grades list:
+        if current grade equals value being counted
+            add ONE to counter for value
 
     Display counter for this value
