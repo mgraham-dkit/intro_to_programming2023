2aa0212493e4a4eab68f4d46742ef8c85504cf77
michelle <michelle.graham@dkit.ie>
2024-04-12 13:07:51 +0100

    Added exercise outline to the sample code

--- S2_Week10 - GUIs/basic_login.py
+++ S2_Week10 - GUIs/basic_login.py
@@ -1,8 +1,22 @@
 from tkinter import *
 
+'''
+    EXERCISE:
+        1) Amend this code to display "Status: Login failed" where the user
+        attempts to log in and fails
+        2) Amend the code to check if the text field actually contained text
+        when the user tried to log in. Display "Error: You must enter a username"
+        if nothing was entered.
+        Hint: If there is text in the list, the string you get out of it
+        will be longer than 0.
+        3) Amend the GUI to contain a password entry field and corresponding label.
+        Update your login() command function to check if both the username
+        AND password entered match the credentials stored in the user tuple,
+        not just the username.
+'''
 
 def login():
-    print("I'm broken!!!")
+    print("I'm running!!!")
     entered_username = login_field.get()
     if entered_username == user[0]:
         status_label["text"] = "Status: Logged in!"
